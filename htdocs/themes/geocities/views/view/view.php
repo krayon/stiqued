c5ec2c2348766304b070b6e20ea57c2a9c5dbb96
PeGaSuS <droider.pc@gmail.com>
2022-12-17 19:20:17 +0100

    Update view.php
    Disable replies if `disable_replies` is set to `true`

--- htdocs/themes/geocities/views/view/view.php
+++ htdocs/themes/geocities/views/view/view.php
@@ -104,9 +104,13 @@ if(isset($replies) and !empty($replies)){
 <?php echo $pages;
 }
 
+if(!$this->config->item('disable_replies')){
 	$reply_form['page']['title'] = lang('paste_replyto') . ' "' . $title . '"';
 	$reply_form['page']['instructions'] = lang('paste_replyto_desc');
 	$this->load->view('defaults/paste_form', $reply_form); ?>
+	$this->load->view('defaults/paste_form', $reply_form);
+} ?>
 
+</section>
 
 <?php $this->load->view('view/view_footer'); ?>
