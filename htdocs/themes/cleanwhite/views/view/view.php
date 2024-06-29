6cb39eb2c000f8cdac65ab81ec562c7b4b37de16
Krayon <krayon.git@qdnx.org>
2024-06-29 15:05:44 +1000

    `disable_replies` support for remaining themes

--- htdocs/themes/cleanwhite/views/view/view.php
+++ htdocs/themes/cleanwhite/views/view/view.php
@@ -115,9 +115,11 @@ if(isset($insert)){
 		<?php }?>
 		
 		<?php 
+                    if(!$this->config->item('disable_replies')){
 			$reply_form['page']['title'] = lang('paste_replyto') . ' "' . $title . '"';
 			$reply_form['page']['instructions'] = lang('paste_replyto_desc');
-		$this->load->view('defaults/paste_form', $reply_form); ?>
+		$this->load->view('defaults/paste_form', $reply_form);
+                    } ?>
 	</div>
 
 </div>
